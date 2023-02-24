c0ee13f8b18c3387bbce71d8107d7059a3d084b0
Dani <d.tarazoma@geekshubs.com>
2023-02-24 12:43:12 +0100

    feat: create product

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Product } = require('./models/index')
 const db = require('./db.js');
 require('dotenv').config()
 
@@ -12,8 +13,28 @@ app.get('/welcome', (req, res) => {
     return res.send("Bienvenido a mi app")
 })
 
-db.then(()=>{
+app.post('/products', async(req, res) => {
+    // Recuperamos la informacion a traves de la req
+    // const name = req.body.name;
+    // const description = req.body.description;
+    // const price = req.body.price;
+
+    const { name, description, price } = req.body;
+
+    const newProduct = {
+        name: name,
+        description: description,
+        price: price
+    }
+
+    // Guardar la informacion
+    const product = await Product.create(newProduct)
+
+    return res.json(product)
+})
+
+db.then(() => {
     //Starting server
-        app.listen(PORT, ()=> console.log("Server on port " + PORT));
-    })
-    .catch((err)=> console.log(err.message));   
+    app.listen(PORT, () => console.log("Server on port " + PORT));
+})
+    .catch((err) => console.log(err.message));   
