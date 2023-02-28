8acd52966f03a6402d63f8a61a1343e8dbea624e
Dani <d.tarazoma@geekshubs.com>
2023-02-28 13:44:19 +0100

    feat: add verify token

--- views/productRoutes.js
+++ views/productRoutes.js
@@ -1,10 +1,11 @@
 const productController = require('../controllers/productController');
+const verifyToken = require('../middlewares/verifyToken');
 const router = require('express').Router();
 
-router.post('/products', productController.createProduct)
+router.post('/products', verifyToken,productController.createProduct)
 router.get('/products', productController.getProducts)
 router.get('/products/:id', productController.getProductById)
-router.delete('/products/:id', productController.deleteProductById)
-router.put('/products/:id', productController.updateProductById)
+router.delete('/products/:id', verifyToken, productController.deleteProductById)
+router.put('/products/:id', verifyToken, productController.updateProductById)
 
 module.exports = router;
